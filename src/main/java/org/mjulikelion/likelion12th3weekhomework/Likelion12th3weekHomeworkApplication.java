e600592ebaa5f25567f51b43574fb2416c5198a5
juryheed <rabitangl20@naver.com>
2024-04-16 20:53:16 +0900

    init String boot

---
+++ src/main/java/org/mjulikelion/likelion12th3weekhomework/Likelion12th3weekHomeworkApplication.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.likelion12th3weekhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Likelion12th3weekHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Likelion12th3weekHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mjulikelion/likelion12th3weekhomework/Likelion12th3weekHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.likelion12th3weekhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Likelion12th3weekHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
